bf69b54b550c146bcbac911380cb1a9a6b5d113a
Tude Nugraha <nugrahaajus31@gmail.com>
2026-06-03 04:53:43 +0000

    Redirect homepage to login

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/login');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
